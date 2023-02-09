192e67cc08c21b4ea3cde65a9a85dd0e36e86878
Aibar Shora <aybarshora@gmail.com>
2023-02-10 01:24:49 +0600

    Created minimal console interface with empty methods. Changed head image.

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,49 @@ public class Main extends JFrame{
         setResizable( false );
         pack();
 
-        setTitle( "SNAKE ONLINE GAME!" );
+        setTitle( "Snake" );
         setLocationRelativeTo( null );
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     }
-
     public static void main( String[] args ) {
-        EventQueue.invokeLater(() -> {
-            JFrame ex = new Main();
-            ex.setVisible( true );
-        });
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to online SNAKE GAME!");
+        System.out.println("[1] --> Start Game");
+        System.out.println("[2] --> Sign Up");
+        System.out.println("[3] --> Rating of Players");
+        System.out.println("[4] --> open Admin page");
+        System.out.println("[5] --> close app");
+        System.err.println("enter key number: ");
+
+        do {
+            int n = in.nextInt();
+            switch (n) {
+                case 1 -> EventQueue.invokeLater(() -> {
+                    JFrame ex = new Main();
+                    ex.setVisible(true);
+                });
+                case 2 -> SignUp();
+                case 3 -> RatingShow();
+                case 4 -> Settings();
+                case 5 -> Exit();
+                default -> System.out.println("invalid number");
+            }
+        } while (true);
     }
+
+    private static void SignUp(){
+        System.out.println("Sign Up to the game!");
+    }
+
+    private static void RatingShow(){
+        System.out.println("Showing Rating...");
+    }
+    private static void Settings(){
+        System.out.println("Settings...");
+    }
+    private static void Exit(){
+        System.out.println("Closing game...");
+    }
+
+
 }
