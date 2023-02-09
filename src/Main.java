11c932b2c28942be1ac463707be1588ceec95ec1
sultok <sultok.003@gmail.com>
2023-02-10 00:02:39 +0600

    Created Map class with implementation of all snake game logics. Still have some bugs but it will be solved later

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import SnakeLogic.Map;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+import java.util.Scanner;
+public class Main extends JFrame{
+    public Main() {
+        snakeUI();
+    }
+
+    private void snakeUI() {
+        add( new Map() );
+
+        setResizable( false );
+        pack();
+
+        setTitle( "SNAKE ONLINE GAME!" );
+        setLocationRelativeTo( null );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
+
+    public static void main( String[] args ) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Main();
+            ex.setVisible( true );
+        });
     }
 }
